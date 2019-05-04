fb74d342fcab0d97778eb25a90f7423f313c1fc8
cxx226 <43131581+cxx226@users.noreply.github.com>
2019-05-04 16:14:23 +0800

    Update manager.h

--- manager.h
+++ manager.h
@@ -3,12 +3,11 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
-#include"Table.h"
 #include"sql.h"
+#include"Database.h"
 using namespace std;
 
 
-class Database;
 /*Database 类是具有表存取功能的数据库类型，暂时未定义，应修改为相应类型
  *Manager类是管理database的数据类型
  *getTB(string name)为Database类中函数，获取指向名为name的表的指针*/
@@ -19,7 +18,7 @@ public:
 	void CreateTable(SQLCreateTable& statement);
 	void ShowDatabases();
 	void ShowTables();
-	void ShowColumns();
+	void ShowColumns(SQLShowColumns &statement);
 	void DropDatabase(SQLDropDatabase& statement);
 	void DropTable(SQLDropTable& statement);
 	void Use(SQLUse& statement);
@@ -27,12 +26,11 @@ public:
 	void Select(SQLSelect& statement);
 	void Delete(SQLDelete& statement);
 	void Update(SQLUpdate& statement);
-	Database *GetDB();//返回当前数据库的指针
-	Database *GetDB(string db);//返回名为db的数据库的指针
+	DataBase *GetDB();//返回当前数据库的指针
+	DataBase *GetDB(string db);//返回名为db的数据库的指针
 	Manager();
 private:
 	string current_db;
-	vector<Database> databases;
+	vector<DataBase> databases;
 	int num;
-
 };
