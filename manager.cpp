d8d48e248757db7c6eb44fa636e5c52dc70c924a
cxx226 <43131581+cxx226@users.noreply.github.com>
2019-05-04 16:15:13 +0800

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -1,32 +1,33 @@
 #include"manager.h"
 
+
 Manager::Manager():num(0){}
 
 
 void Manager::CreateDatabase(SQLCreateDatabase& statement){
-	cout << "Creating Database: " << statement.get_db_name() << endl;
-	Database *temp=new Database(statement.get_db_name());//此处应修改为相应构造函数
+	cout << "Creating DataBase: " << statement.get_db_name() << endl;
+	DataBase *temp=new DataBase(statement.get_db_name());//此处应修改为相应构造函数
 	databases.push_back(*temp);
 	num++;
 }
 
-Database* Manager::GetDB(){
+DataBase* Manager::GetDB(){
 	for(int i=0;i<num;i++)
-		if(databases[i].name=current_db)
+		if(databases[i].get_db_name()==current_db)
 			return &(databases[i]);
 	return NULL;
 }
 
-Database* Manager::GetDB(string db){
+DataBase* Manager::GetDB(string db){
 	for(int i=0;i<num;i++)
-		if(databases[i].name=db)
+		if(databases[i].get_db_name()==db)
 			return &(databases[i]);
 	return NULL;
 }
 void Manager::CreateTable(SQLCreateTable& statement){
 	cout << "Creating table: " << statement.get_tb_name() << endl;
 
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	db->CreateTable(statement);
 }
 
@@ -36,7 +37,7 @@ void Manager::ShowDatabases()
 	if (num == 0)
 	{
 		cout << "No databases exist now." << endl;
-		cout << "Use 'create database' command to create a new database." << endl;
+		cout << "Use 'create DataBase' command to create a new DataBase." << endl;
 		return;
 	}
 	cout<< "The number of databases is"<<num<<endl;
@@ -47,7 +48,7 @@ void Manager::ShowDatabases()
 void Manager::ShowTables()
 {
 	if (current_db.size() == 0) return;
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	if (db == NULL) return;
 	if (db->GetTables().size() == 0)
 	{
@@ -58,13 +59,13 @@ void Manager::ShowTables()
 	cout << "| " << setw(22) << "Tables_in_"+ current_db << " |" << endl;
 	
 	for (auto tb = db->GetTables().begin(); tb != db->GetTables().end(); tb++)
-		cout << "| " << setw(22) << (*tb).get_tb_name() << " |" << endl;
+		cout << "| " << setw(22) << (*tb)->name << " |" << endl;
 	
 }
 
-void Manager::ShowColumns(){
+void Manager::ShowColumns(SQLShowColumns &statement){
 	if (current_db.size() == 0) return;
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	if (db == NULL) return;
 	if (db->GetTables().size() == 0)
 	{
@@ -72,16 +73,16 @@ void Manager::ShowColumns(){
 		cout << "Use 'create table' command to create a new table." << endl;
 		return;
 	}
-	Table *tb=db->getTables();
+	Table *tb=db->getTB(statement.get_tb_name());
 	/*列出制定表项的各项信息，包括属性、属性类型、主键 信息。*/
-
+	cout<<tb;
 
 }
 
 
 void Manager::DropDatabase(SQLDropDatabase& statement)
 {
-	cout << "Droping database: " << statement.get_db_name() << endl;
+	cout << "Droping DataBase: " << statement.get_db_name() << endl;
 	
 	//to be finished: delete statement.get_db_name; 
 	
@@ -95,10 +96,10 @@ void Manager::DropTable(SQLDropTable& statement)
 	cout << "Droping table: " << statement.get_tb_name() << endl;
 	if (current_db.length() == 0) return;
 
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	if (db == NULL) return;
 
-	Table *tb = db->GetTable(statement.get_tb_name());
+	Table *tb = db->getTB(statement.get_tb_name());
 	if (tb == NULL) return;
 	
 	db->DropTable(statement.get_tb_name());
@@ -106,7 +107,7 @@ void Manager::DropTable(SQLDropTable& statement)
 
 void Manager::Use(SQLUse& statement)
 {
-	Database *db = GetDB(statement.get_db_name());
+	DataBase *db = GetDB(statement.get_db_name());
 	if (db == NULL) return;
 	current_db = statement.get_db_name();
 }
@@ -114,7 +115,7 @@ void Manager::Use(SQLUse& statement)
 void Manager::Insert(SQLInsert& statement)
 {
 	if (current_db.length() == 0) return;
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	if (db == NULL) return;
 	
 	string tb_name=statement.get_tb_name();
@@ -125,7 +126,7 @@ void Manager::Insert(SQLInsert& statement)
 void Manager::Select(SQLSelect& statement)
 {
 	if (current_db.length() == 0) return;
-	Table *tb = GetDB()->GetTB(statement.get_tb_name());
+	Table *tb = GetDB()->getTB(statement.get_tb_name());
 	if (tb == NULL) return;
 	WhereClause c(statement.s,statement.o);
 	tb->select(statement.attrFilter,c);	
@@ -134,7 +135,7 @@ void Manager::Select(SQLSelect& statement)
 void Manager::Delete(SQLDelete& statement)
 {
 	if (current_db.length() == 0) return;
-	Table *tb = GetDB()->GetTable(statement.get_tb_name());
+	Table *tb = GetDB()->getTB(statement.get_tb_name());
 	if (tb == NULL) return;
 	WhereClause c(statement.s,statement.o);
 	tb->del(c);
@@ -143,10 +144,10 @@ void Manager::Delete(SQLDelete& statement)
 void Manager::Update(SQLUpdate& statement)
 {
 	if (current_db.length() == 0) return;
-	Database *db = GetDB();
+	DataBase *db = GetDB();
 	if (db == NULL) return;
 
-	Table *tb = db->GetTB(statement.get_tb_name());
+	Table *tb = db->getTB(statement.get_tb_name());
 	if (tb == NULL) return;
 	WhereClause c(statement.s,statement.o);
 	tb->update(statement.attrNames,statement.vals,c);
