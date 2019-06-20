86b1275edff2a4d2a97e6670fe7937c7d8b85c6b
cxx226 <43131581+cxx226@users.noreply.github.com>
2019-06-21 04:17:15 +0800

    add test for union

---
+++ test/union.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE OOP;
+USE OOP;
+CREATE TABLE oop_info(stu_id INT NOT NULL, PRIMARY KEY(stu_id), stu_name CHAR);
+SHOW columns from oop_info;
+INSERT INTO oop_info(stu_id, stu_name) VALUES (2018011243, "a");
+INSERT INTO oop_info(stu_id, stu_name) VALUES (2018011344, "b");
+INSERT INTO oop_info(stu_id, stu_name) VALUES (2018011445, "c");
+CREATE TABLE oop_info1(stu_id INT NOT NULL, PRIMARY KEY(stu_id), stu_name CHAR);
+SHOW columns from oop_info1;
+INSERT INTO oop_info1(stu_id, stu_name) VALUES (2018011243, "a");
+INSERT INTO oop_info1(stu_id, stu_name) VALUES (2018011777, "b");
+INSERT INTO oop_info1(stu_id, stu_name) VALUES (2018011666, "c");
+SELECT stu_id from oop_info union SELECT stu_id from oop_info1;
+SELECT stu_id from oop_info union all SELECT stu_id from oop_info1;
+DROP DATABASE OOP;
